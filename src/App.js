1fa1c28e75bf592e455a0f88c6a4935499f5026d
203424 <203424@ids.upchiapas.edu.mx>
2022-03-14 20:28:53 -0600

    Actualizacion de App.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,24 @@ import Login from './components/UsuarioLogin/Login';
 import Menu from './components/Menu/Menu';
 import Register from './components/UsuarioRegistro/Register';
 import FlujoEfectivo from './components/FlujoEfectivo/FlujoEfectivo';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
-import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-<<<<<<< HEAD
-        <Route path='/home/*' element={<Menu/>}>
-          <Route path='registro' element={<Register/>}/>
-        </Route>
-=======
-        <Route path='/home' element={<Menu/>}/>
-        <Route path='/registro' element={<Register/>}/>
-        <Route path='/flujo' element={<FlujoEfectivo/>}/>
-
->>>>>>> b34455aa7fdc055e6e43ed5d12232331317bd05b
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/home/*' element={<Menu />}>
+                    <Route path='registro' element={<Register />} />
+                </Route>
+                <Route path='/flujo' element={<FlujoEfectivo />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
